8404261b24493a2992c940281f01e3663bbb3ae0
mateushmteteu-a11y <mateushmteteu@gmail.com>
2026-05-14 14:39:11 -0300

    Update Codigo_matheus.py

--- Codigo_matheus.py
+++ Codigo_matheus.py
@@ -1,11 +1,13 @@
 import mysql.connector
 
-def conectar():
+def criar_conexão():
     return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="Senac2026",
-        database="Escola"
+        host = "127.0.0.1",
+        user = "root",
+        password = "Senac2026",
+        database = "Escola",
     )
 
-def 
+def cadastrar_aluno():
+    nome_do_aluno = input("Nome do aluno")
+    
