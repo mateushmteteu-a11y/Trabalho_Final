0bbdd6ab10dd3fb747ffb5f4a0b145511df9a362
AntonioHoffmann <antoniohoffmann2010@gmail.com>
2026-05-18 14:11:45 -0300

    Update Codigo_Bruno.py
    atualizado2

--- Codigo_Bruno.py
+++ Codigo_Bruno.py
@@ -42,7 +42,7 @@ def cadastrar_aluno():
     alunos.append(aluno)
     print("Aluno cadastrado com sucesso!")
 
-#LISTAR ALUNOS
+#LISTAR ALUNOS:
 def listar_alunos():
     if len(alunos) == 0:
         print("Nunhum aluno cadastrado.")
@@ -51,4 +51,45 @@ def listar_alunos():
     for i, aluno in enumerate(alunos):
 
         média = calcular_média(aluno["notas"])
-        status = situação(média)
+        status = situação(média) 
+        print("\n---------------------")
+        print(f"ID: {i}")
+        print(f"Nome: {aluno['nome']}")
+        print(f"Idade: {aluno['idade']}")
+        print(f"Turma: {aluno['turma']}")
+        print(f"Notas: {aluno['notas']}")
+        print(f"Média: {média:.2f}")
+        print(f"Situação: {status}")
+
+#EDITAR ALUNOS:
+def editar_aluno():
+    listar_alunos()
+ 
+    id_aluno = int(input("Digite o ID do aluno: "))
+ 
+    if id_aluno < 0 or id_aluno >= len(alunos):
+        print("Aluno não encontrado.")
+        return
+ 
+    alunos[id_aluno]["nome"] = input("Novo nome: ")
+    alunos[id_aluno]["idade"] = int(input("Nova idade: "))
+    alunos[id_aluno]["turma"] = input("Nova turma: ")
+ 
+    print("Aluno atualizado!")
+
+
+#REMOVER ALUNOS:
+def remover_aluno():
+    listar_alunos()
+ 
+    id_aluno = int(input("Digite o ID do aluno: "))
+ 
+    if id_aluno < 0 or id_aluno >= len(alunos):
+        print("Aluno não encontrado.")
+        return
+ 
+    alunos.pop(id_aluno)
+ 
+    print("Aluno removido!")
+ 
+ 
