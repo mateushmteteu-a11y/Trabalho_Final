84fc8be518766bcd2a66f5d12ffd13e53453b0a3
AntonioHoffmann <antoniohoffmann2010@gmail.com>
2026-05-18 14:20:49 -0300

    Update Codigo_Bruno.py
    atualizado3

--- Codigo_Bruno.py
+++ Codigo_Bruno.py
@@ -92,4 +92,81 @@ def remover_aluno():
  
     print("Aluno removido!")
  
+ # ADICIONAR NOTAS:
+def adicionar_notas():
+    listar_alunos()
  
+    id_aluno = int(input("Digite o ID do aluno: "))
+ 
+    if id_aluno < 0 or id_aluno >= len(alunos):
+        print("Aluno não encontrado.")
+        return
+ 
+    quantidade = int(input("Quantas notas deseja adicionar? "))
+ 
+    for i in range(quantidade):
+        nota = float(input(f"Digite a nota {i+1}: "))
+        alunos[id_aluno]["notas"].append(nota)
+ 
+    print("Notas adicionadas!")
+
+# REMOVER NOTA:
+def remover_nota():
+    listar_alunos()
+ 
+    id_aluno = int(input("Digite o ID do aluno: "))
+ 
+    if id_aluno < 0 or id_aluno >= len(alunos):
+        print("Aluno não encontrado.")
+        return
+ 
+    print(alunos[id_aluno]["notas"])
+ 
+    indice = int(input("Digite o índice da nota: "))
+ 
+    if indice < 0 or indice >= len(alunos[id_aluno]["notas"]):
+        print("Índice inválido.")
+        return
+ 
+    alunos[id_aluno]["notas"].pop(indice)
+ 
+    print("Nota removida!")
+ 
+# MENU
+while True:
+ 
+    print("\n===== SISTEMA ESCOLAR =====")
+    print("1 - Cadastrar aluno")
+    print("2 - Listar alunos")
+    print("3 - Editar aluno")
+    print("4 - Remover aluno")
+    print("5 - Adicionar notas")
+    print("6 - Remover nota")
+    print("0 - Sair")
+ 
+    opcao = input("Escolha uma opção: ")
+ 
+    if opcao == "1":
+        cadastrar_aluno()
+ 
+    elif opcao == "2":
+        listar_alunos()
+ 
+    elif opcao == "3":
+        editar_aluno()
+ 
+    elif opcao == "4":
+        remover_aluno()
+ 
+    elif opcao == "5":
+        adicionar_notas()
+ 
+    elif opcao == "6":
+        remover_nota()
+ 
+    elif opcao == "0":
+        print("Sistema encerrado.")
+        break
+ 
+    else:
+        print("Opção inválida!")
