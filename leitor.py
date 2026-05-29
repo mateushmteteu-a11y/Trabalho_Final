30ae02e34f935b389a1bf84e8bb3702d986d98db
PedroFelipeLima <pedrofelipeferreiralimaaaaaa@gmail.com>
2026-05-29 08:54:48 -0300

    coisei os coisos

--- leitor.py
+++ leitor.py
@@ -7,14 +7,12 @@ def lista_alunos():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Aluno")
     resultado = cursor.fetchall()
-    for aluno in resultado:
-        if not resultado:
+    if not resultado:
             print("Nenhum aluno encontrado.")
-
-        else:
+    else:
+        for aluno in resultado:
             print(f"""ID: {aluno[0]} | Nome: {aluno[1]} | Idade: {aluno[2]} | Turma: {aluno[3]} | Situação de matematica: {aluno[4]} | Situação de portugues: {aluno[5]}
------------------------------
-                """)
+-----------------------------""")
     cursor.close()
     conn.close()
 def lista_matematica():
@@ -25,11 +23,10 @@ def lista_matematica():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Matematica")
     resultado = cursor.fetchall()
-    for aluno in resultado:
-        if not resultado:
-            print("Nenhum aluno encontrado.")
-
-        else:
+    if not resultado:
+            print("Nenhuma nota encontrada.")
+    else:
+        for aluno in resultado:
             print(f"""ID: {aluno[0]} | Nome: {aluno[2]} | Turma: {aluno[3]} | Nota 1 de matematica: {aluno[3]} | Nota 2 de matematica: {aluno[4]} | Nota 3 de matematica: {aluno[5]} | Soma de matematica: {aluno[6]} | Media de matematica: {aluno[7]} | Situação de matematica: {aluno[8]}
 -----------------------------
                 """)
@@ -43,11 +40,10 @@ def lista_portugues():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Portugues")
     resultado = cursor.fetchall()
-    for aluno in resultado:
-        if not resultado:
-            print("Nenhum aluno encontrado.")
-
-        else:
+    if not resultado:
+            print("Nenhum nota encontrada.")
+    else:
+        for aluno in resultado:
             print(f"""ID: {aluno[0]} | Nome: {aluno[2]} | Turma: {aluno[3]} | Nota 1 de portugues: {aluno[3]} | Nota 2 de portugues: {aluno[4]} | Nota 3 de portugues: {aluno[5]} | Soma de portugues: {aluno[6]} | Media de portugues: {aluno[7]} | Situação de portugues: {aluno[8]}
 -----------------------------
                 """)
